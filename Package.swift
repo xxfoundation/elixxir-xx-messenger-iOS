c474540c8e9b354b269b7459e27e04082aa30d03
Dariusz Rybicki <dariusz@elixxir.io>
2022-07-18 13:25:46 +0100

    Add CustomDump dependency

--- Package.swift
+++ Package.swift
@@ -71,7 +71,8 @@ let package = Package(
         .package(url: "https://git.xx.network/elixxir/client-ios-db.git", .upToNextMajor(from: "1.0.5")),
         .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "8.10.0")),
         .package(url: "https://github.com/darrarski/Shout.git", revision: "df5a662293f0ac15eeb4f2fd3ffd0c07b73d0de0"),
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git",.upToNextMajor(from: "0.32.0"))
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git",.upToNextMajor(from: "0.32.0")),
+        .package(url: "https://github.com/pointfreeco/swift-custom-dump.git", .upToNextMajor(from: "0.5.0")),
     ],
     targets: [
         .target(
@@ -872,6 +873,7 @@ let package = Package(
                 name: "CollectionViewTests",
                 dependencies: [
                     .target(name: "CollectionView"),
+                    .product(name: "CustomDump", package: "swift-custom-dump"),
                 ]
             ),
     ]
