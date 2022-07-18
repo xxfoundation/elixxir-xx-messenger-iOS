1186a3cbaea9d107e5fab13fd875afe6e3877f6f
Dariusz Rybicki <dariusz@elixxir.io>
2022-07-18 13:08:10 +0100

    Refactor

--- Sources/CollectionView/CellFactory.swift
+++ Sources/CollectionView/CellFactory.swift
@@ -2,6 +2,10 @@ import UIKit
 
 public struct CellFactory<Model> {
   public struct Registrar {
+    public init(register: @escaping (UICollectionView) -> Void) {
+      self.register = register
+    }
+
     public var register: (UICollectionView) -> Void
 
     public func callAsFunction(in view: UICollectionView) {
@@ -10,19 +14,20 @@ public struct CellFactory<Model> {
   }
 
   public struct Builder {
-    public var buildCell: (Model, UICollectionView, IndexPath) -> UICollectionViewCell?
+    public init(build: @escaping (Model, UICollectionView, IndexPath) -> UICollectionViewCell?) {
+      self.build = build
+    }
+
+    public var build: (Model, UICollectionView, IndexPath) -> UICollectionViewCell?
 
     public func callAsFunction(
       for model: Model,
       in view: UICollectionView,
       at indexPath: IndexPath
     ) -> UICollectionViewCell? {
-      buildCell(model, view, indexPath)
+      build(model, view, indexPath)
     }
   }
-
-  public var register: Registrar
-  public var build: Builder
 
   public init(
     register: Registrar,
@@ -31,6 +36,9 @@ public struct CellFactory<Model> {
     self.register = register
     self.build = build
   }
+
+  public var register: Registrar
+  public var build: Builder
 }
 
 extension CellFactory {
