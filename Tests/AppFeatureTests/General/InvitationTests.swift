3b20d44b6fe5dbf07701fc5a0accc84b27560408
Bruno Muniz Azevedo Filho <bruno@elixxir.io>
2022-11-29 16:03:11 -0300

    fix identation of invitation tests

--- Tests/AppFeatureTests/General/InvitationTests.swift
+++ Tests/AppFeatureTests/General/InvitationTests.swift
@@ -1,35 +1,35 @@
 import XCTest
 
-@testable import App
+@testable import AppFeature
 
 final class AppDelegateTests: XCTestCase {
-    func test_invitationUniversalLink() {
-        XCTAssertNil(getUsernameFromInvitationDeepLink(
-            URL(string: "https://elixxir.io/connecting?username=some")!
-        ))
+  func test_invitationUniversalLink() {
+    XCTAssertNil(getUsernameFromInvitationDeepLink(
+      URL(string: "https://elixxir.io/connecting?username=some")!
+    ))
 
-        XCTAssertNil(getUsernameFromInvitationDeepLink(
-            URL(string: "http://elixxir.io/connect?username=some")!
-        ))
+    XCTAssertNil(getUsernameFromInvitationDeepLink(
+      URL(string: "http://elixxir.io/connect?username=some")!
+    ))
 
-        XCTAssertNil(getUsernameFromInvitationDeepLink(
-            URL(string: "https://io.elixxir/connect?username=some")!
-        ))
+    XCTAssertNil(getUsernameFromInvitationDeepLink(
+      URL(string: "https://io.elixxir/connect?username=some")!
+    ))
 
-        XCTAssertEqual(getUsernameFromInvitationDeepLink(
-            URL(string: "https://elixxir.io/connect?username=brad")!
-        ), "brad")
+    XCTAssertEqual(getUsernameFromInvitationDeepLink(
+      URL(string: "https://elixxir.io/connect?username=brad")!
+    ), "brad")
 
-        XCTAssertNil(getUsernameFromInvitationDeepLink(
-            URL(string: "https://elixxir.io/connect?password=value")!
-        ))
+    XCTAssertNil(getUsernameFromInvitationDeepLink(
+      URL(string: "https://elixxir.io/connect?password=value")!
+    ))
 
-        XCTAssertNil(getUsernameFromInvitationDeepLink(
-            URL(string: "https://elixxir.io/connect?usernamer=some")!
-        ))
+    XCTAssertNil(getUsernameFromInvitationDeepLink(
+      URL(string: "https://elixxir.io/connect?usernamer=some")!
+    ))
 
-        XCTAssertNotEqual(getUsernameFromInvitationDeepLink(
-            URL(string: "https://elixxir.io/connect?username=anderson")!
-        ), "silva")
-    }
+    XCTAssertNotEqual(getUsernameFromInvitationDeepLink(
+      URL(string: "https://elixxir.io/connect?username=anderson")!
+    ), "silva")
+  }
 }
