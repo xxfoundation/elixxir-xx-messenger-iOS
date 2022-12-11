75efb53840364f35edbfb4691ce17ff575888ffe
Bruno Muniz Azevedo Filho <bruno@elixxir.io>
2022-12-11 03:18:33 -0300

    Fixes size of tappable area for profile button on menu

--- Sources/MenuFeature/Views/MenuHeaderView.swift
+++ Sources/MenuFeature/Views/MenuHeaderView.swift
@@ -3,52 +3,50 @@ import Shared
 import AppResources
 
 final class MenuHeaderView: UIView {
-    let nameButton = UIButton()
-    let scanButton = UIButton()
-    let stackView = UIStackView()
-    let avatarView = AvatarView()
-    let verticalStackView = UIStackView()
+  let textLabel = UILabel()
+  let scanButton = UIButton()
+  let avatarView = AvatarView()
+  let profileButton = UIControl()
 
-    init() {
-        super.init(frame: .zero)
+  init() {
+    super.init(frame: .zero)
 
-        let helloLabel = UILabel()
-        helloLabel.text = Localized.Menu.title
-        helloLabel.textColor = Asset.neutralWeak.color
-        helloLabel.font = Fonts.Mulish.semiBold.font(size: 14.0)
+    textLabel.numberOfLines = 0
+    scanButton.layer.cornerRadius = 14
+    avatarView.isUserInteractionEnabled = false
+    scanButton.backgroundColor = Asset.neutralBody.color
+    scanButton.setImage(Asset.menuScan.image, for: .normal)
 
-        nameButton.titleLabel?.font = Fonts.Mulish.bold.font(size: 18.0)
-        nameButton.setTitleColor(Asset.neutralLine.color, for: .normal)
+    addSubview(scanButton)
+    addSubview(profileButton)
+    profileButton.addSubview(avatarView)
+    profileButton.addSubview(textLabel)
 
-        let spacingView = UIView()
-        verticalStackView.axis = .vertical
-        verticalStackView.addArrangedSubview(spacingView)
-        verticalStackView.addArrangedSubview(helloLabel)
-        verticalStackView.addArrangedSubview(nameButton.pinning(at: .left(0)))
-
-        verticalStackView.setCustomSpacing(15, after: spacingView)
-        verticalStackView.setCustomSpacing(5, after: helloLabel)
-
-        scanButton.layer.cornerRadius = 14
-        scanButton.snp.makeConstraints { $0.width.height.equalTo(40) }
-        scanButton.setImage(Asset.menuScan.image, for: .normal)
-        scanButton.backgroundColor = Asset.neutralBody.color
-
-        stackView.spacing = 15
-        stackView.addArrangedSubview(avatarView)
-        stackView.addArrangedSubview(verticalStackView)
-        stackView.addArrangedSubview(scanButton.pinning(at: .top(0)))
-
-        addSubview(stackView)
-
-        avatarView.snp.makeConstraints { $0.width.height.equalTo(70) }
-        stackView.snp.makeConstraints { $0.edges.equalToSuperview() }
+    profileButton.snp.makeConstraints {
+      $0.top.equalToSuperview()
+      $0.left.equalToSuperview()
+      $0.bottom.equalToSuperview()
+      $0.right.lessThanOrEqualTo(scanButton.snp.left)
     }
-
-    required init?(coder: NSCoder) { nil  }
-
-    func set(username: String, image: Data? = nil) {
-        nameButton.setTitle(username, for: .normal)
-        avatarView.setupProfile(title: username, image: image, size: .large)
+    avatarView.snp.makeConstraints {
+      $0.top.equalToSuperview()
+      $0.left.equalToSuperview()
+      $0.width.equalTo(70)
+      $0.height.equalTo(70)
+      $0.bottom.equalToSuperview()
     }
+    scanButton.snp.makeConstraints {
+      $0.top.equalToSuperview()
+      $0.right.equalToSuperview()
+      $0.width.equalTo(40)
+      $0.height.equalTo(40)
+    }
+    textLabel.snp.makeConstraints {
+      $0.centerY.equalTo(avatarView)
+      $0.left.equalTo(avatarView.snp.right).offset(20)
+      $0.right.equalToSuperview()
+    }
+  }
+
+  required init?(coder: NSCoder) { nil  }
 }
